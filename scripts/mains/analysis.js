ce837cdb1624f4425602f6471c2e04918bb04a02
Daniele Salvatore <daniele.salvatore@fao.org>
2016-01-25 14:06:23 +0100

    Submodules clean up
    fenix-ui-analysis: develop (merged from all features)
    fenix-ui-catalog: develop (from all features)
    fenix-ui-data-management: develop
    fenix-ui-DataEditor: develop ( from featureV2)
    fenix-ui-DSDEditor: develop (from feauteV2)
    fenix-ui-filter: develop (from fx-ui-filter-json)
    fenix-ui-metadata-editor:
        - Tanzi version: master
        - DanieleO version (V2 the only under development): develop !!!! NEW PROJECTS SHOULD USE THIS ONE
    fenix-ui-olap: develop(I copied the current master to develop)
    fenix-ui-reports: develop
    fenix-ui-table-creato: develop
    
    to be tested

--- scripts/mains/analysis.js
+++ scripts/mains/analysis.js
@@ -7,15 +7,11 @@ require([
     '../../submodules/fenix-ui-chart-creator/src/js/paths',
     '../../submodules/fenix-ui-map-creator/src/js/paths',
     '../../submodules/fenix-ui-metadata-viewer/src/js/paths',
-/*
-    '../../submodules/json-editor-faostat-theme/src/js/paths',
-*/
-   /* '../../submodules/faostat-ui-commons/js/paths',*/
     '../../submodules/fenix-ui-common/js/paths',
     '../../submodules/fenix-ui-filter/src/js/paths',
     '../../submodules/fenix-ui-common/js/Compiler',
     '../../submodules/fenix-ui-reports/src/js/paths'
-], function (Catalog, Analysis, Menu, TableCreator, ChartCreator, MapCreator, MetadataViewer, /*FAOSTAT_THEME, faostatCommons,*/ FenixCommons, Filter, Compiler, FenixReports) {
+], function (Catalog, Analysis, Menu, TableCreator, ChartCreator, MapCreator, MetadataViewer, FenixCommons, Filter, Compiler, FenixReports) {
 
     'use strict';
 
@@ -38,13 +34,7 @@ require([
     mapCreatorConfig.baseUrl = '../../submodules/fenix-ui-map-creator/src/js';
 
     var metadataViewerConfig = MetadataViewer;
-    metadataViewerConfig.baseUrl = '../../submodules/fenix-ui-metadata-viewer/src/js/';
-
-   /* var faostatCommonsConfig = faostatCommons;
-    faostatCommonsConfig.baseUrl = '../../submodules/faostat-ui-commons/js';
-
-    var faostatThemeConfig = FAOSTAT_THEME;
-    faostatThemeConfig.baseUrl = '../../submodules/json-editor-faostat-theme/src/js';*/
+    metadataViewerConfig.baseUrl = '../../submodules/fenix-ui-metadata-viewer/js';
 
     var fenixCommonConfig = FenixCommons;
     fenixCommonConfig.baseUrl = '../../submodules/fenix-ui-common/js';
@@ -55,10 +45,9 @@ require([
     var fenixReportsConfig = FenixReports;
     fenixReportsConfig.baseUrl = '../../submodules/fenix-ui-reports/src/js';
 
-    Compiler.resolve([catalogConfig, analysisConfig, menuConfig, tableCreatorConfig, chartCreatorConfig, mapCreatorConfig, metadataViewerConfig,/* faostatCommonsConfig, faostatThemeConfig,*/ fenixCommonConfig, filterConfig, fenixReportsConfig], {
+    Compiler.resolve([catalogConfig, analysisConfig, menuConfig, tableCreatorConfig, chartCreatorConfig, mapCreatorConfig, metadataViewerConfig, fenixCommonConfig, filterConfig, fenixReportsConfig], {
         placeholders: {"FENIX_CDN": "//fenixrepo.fao.org/cdn"},
         config: {
-            waitSeconds : 30,
 
             // Specify the paths of vendor libraries
             paths: {
@@ -67,24 +56,12 @@ require([
                 //'packery': '../lib/packery.1.4.1.min',
 
                 'host/config' : '../../config/config',
-                'fx-menu/templates': '../../scripts/templates',
-
 
                 //Components configuration
                 "fx-cat-br/config/fx-catalog-blank-filter": '../../config/submodules/catalog/blankFilter',
+
                 'fx-ana/config/services' : '../../config/submodules/analysis/services',
-
-                'fx-cat-br/config/config' : '../../config/submodules/fx-catalog/configAnalisi',
-                'fx-filter/config/config-default' : '../../config/submodules/filter/config',
-
-
-                // METADATA VIEWER
-                'fx-mdviewer/config/': '../../config/submodules/fx-md-viewer/',
-                'fx-mdviewer/config/config': '../../config/submodules/fx-md-viewer/config',
-
-
-                'fx-report/config/md-export/config' : '../../config/submodules/fx-report/md-export/config',
-
+                'fx-filter/config/config-default' : '../../config/submodules/filter/config'
 
             },
 
